75f68b7a99ca5102c5dfb97d7ff9966d2565741e
Diksha Tiwari <53514258+dikshatiwari226@users.noreply.github.com>
2025-01-07 17:36:00 +0000

    basics loop

--- 03_Function/06_JavascriptCodeExecution.js
+++ 03_Function/06_JavascriptCodeExecution.js
@@ -1,4 +1,4 @@
-Javascript Execution Context
+// Javascript Execution Context
 
 let var1 = 10
 let val2 = 5
@@ -10,20 +10,24 @@ function addNum(num1, num2){
 let result1 = addNum(val1, val2)
 let result2 = addNum(10, 2)
 
-Execution Phase
-1) Global Execution this 
-2) Memory Phase
+// Execution Phase
+// 1) Global Execution this 
+// 2) Memory Phase
    val1 => undefined
    val2 => undefined
    addNum => defination
    result1 => undefined
    result2 => undefined
 
-3) Execution Phase
+// 3) Execution Phase
     val1 ->10
     val2 ->5
     add Num -> new variable environment + execution thread
 
-Memory Phase
-var1 -> undefined
-val2 -> undefined
+    result1 = 15
+    result2 = 
+
+// Memory Phase          //Execution context
+var1 -> undefined          // num1 -> 10
+val2 -> undefined          // num2 ->5
+total -> undefined         // total -> 15
